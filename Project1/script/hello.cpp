8d4facee0c6fc79b0f320b567531ed5123277d85
root <disneyonjupiter@yahoo.com>
2019-02-13 18:34:12 +0000

    First commit

--- Project1/script/hello.cpp
+++ Project1/script/hello.cpp
@@ -3,14 +3,15 @@
 namespace gazebo
 {
   class WorldPluginMyRobot : public WorldPlugin
-   {
-     public: WorldPluginMyRobot() : WorldPlugin()
-             {
-               printf("Welcome to the world!\n");
-             }
-     public: void Load(physics::WorldPtr _world, sdf::ElementPtr _sdf)
-              {
-              }
-    };
-     GZ_REGISTER_WORLD_PLUGIN(WorldPluginMyRobot)
+  {
+    public: WorldPluginMyRobot() : WorldPlugin()
+            {
+              printf("Welcome to the world!\n");
+            }
+
+    public: void Load(physics::WorldPtr _world, sdf::ElementPtr _sdf)
+            {
+            }
+  };
+  GZ_REGISTER_WORLD_PLUGIN(WorldPluginMyRobot)
 }
